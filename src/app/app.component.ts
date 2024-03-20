4bf0b3e05d57d26e85c2ad5ceff3ade7476465d1
wally <oaowally123@gmail.com>
2024-03-20 21:05:07 +0800

    內嵌繫結 (Interpolation)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Page1Component } from "./page1/page1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Page1Component]
 })
 export class AppComponent {
-  title = 'demo1';
+  title = 'wally';
+  url='http://blog.miniasp.com/';
+  constructor() {
+    setTimeout(() => {
+      this.title = 'Wally is good boy';
+    }, 3000);
+  }
+
 }
