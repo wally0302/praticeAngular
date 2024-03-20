df6da70e6a2137fa74d973132f3f94cff7627f3d
wally <oaowally123@gmail.com>
2024-03-20 22:53:48 +0800

    雙向繫結 (Two-way Binding) 

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { Page1Component } from "./page1/page1.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Page1Component]
+    imports: [RouterOutlet, Page1Component, FormsModule]
 })
 export class AppComponent {
   title = 'wally';
   url='http://blog.miniasp.com/';
   imageurl='/assets/images/logo.png';
+  keyword = '';
   constructor() {
   }
   changeTitle($altKey: boolean) {
     if ($altKey) {
       this.title = 'change title test';
     }
+  }
+  keywordReset() {
+    this.keyword = '';
   }
 }
