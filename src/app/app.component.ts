ef1e7a9c1506876d3c0ef80555212c00925cc7c2
wally <oaowally123@gmail.com>
2024-03-22 15:55:54 +0800

    範本參考變數修正版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,21 @@ import { HeaderComponent } from "./header/header.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Page1Component, FormsModule,HeaderComponent]
+    imports: [RouterOutlet, Page1Component, FormsModule, HeaderComponent]
 })
 export class AppComponent {
-
+  title = 'wally';
+  url='http://blog.miniasp.com/';
+  imageurl='/assets/images/logo.png';
   keyword = '';
   constructor() {
   }
 
+  changeTitle($altKey: boolean) {
+    if ($altKey) {
+      this.title = 'change title test';
+    }
+  }
   keywordReset() {
     this.keyword = '';
   }
