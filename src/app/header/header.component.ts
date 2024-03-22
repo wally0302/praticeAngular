dcf759aa44d8af91f8c5c9bdc7c1eb078c7adc4b
wally <oaowally123@gmail.com>
2024-03-22 17:04:39 +0800

    屬性型指令 並直接tag修改樣式(文字大小)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; //ngStyle所以要引入CommonModule
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   title = 'wally';
-  url='http://blog.miniasp.com/';
-  imageurl='/assets/images/logo.png';
+  url = 'http://blog.miniasp.com/';
+  imageurl = '/assets/images/logo.png';
+  counter = 0;//計數器
 
   changeTitle($altKey: boolean) {
     if ($altKey) {
       this.title = 'change title test';
     }
+    this.counter++;
+  }
+  //  屬性型指令
+  getStyle() {
+    return {
+      'font-size': (12+this.counter) +'px'
+    };
   }
 }
