a561443107d0b720eea86a8afd06ad9e0bae3e90
wally <oaowally123@gmail.com>
2024-04-09 00:29:54 +0800

    Input()的功用:跨 Component 使用

--- src/app/article/article-header/article-header.component.ts
+++ src/app/article/article-header/article-header.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './article-header.component.css'
 })
 export class ArticleHeaderComponent {
-  @Input()
-  item: any;
 
+  @Input()
+  item: any;//如果只有"item: any;" 代表是只能在 ArticleHeaderComponent 使用，如果要在外部(article-list.component.html )使用，需要加上@Input()
 }
